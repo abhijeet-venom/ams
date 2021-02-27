24c7909a73349a8a065719ef6770640c1634f559
Abhijeet <abhijeetsrivastava3967@gmail.com>
2021-02-27 18:12:42 +0530

    My first commit

---
+++ Appliance Management Demo/src/com/dbconnection/DataBaseConnection.java
@@ -0,0 +1,36 @@
+package com.dbconnection;
+
+import java.sql.*;
+
+public class DataBaseConnection 
+{
+	private static Connection con;
+
+	public static Connection connection()
+	{
+		try
+		{
+			Class.forName("com.mysql.jdbc.Driver");
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ams","root","root");
+		}
+		catch(ClassNotFoundException | SQLException cnf)
+		{
+			
+			cnf.printStackTrace();
+		}
+		return con;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
